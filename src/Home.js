5da34ef006e514f42a0ec72ed1c24866f4d7b686
AbrahamBen <eternelle.vision@outlook.fr>
2023-03-08 14:57:57 +0100

    Adding components

---
+++ src/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return ( 
+        <div className="home">
+            <h2>Notre page d'accueil</h2>
+        </div>
+     );
+}
+ 
+export default Home;
---
+++ src/Navbar.js
@@ -0,0 +1,24 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div>
+        <a href="">AHLIAD BLOG</a>
+      </div>
+      <ul className="liens">
+        <li>
+          <a href="" className="lien">
+            Accueil
+          </a>
+        </li>
+
+        <li>
+          <a href="" className="lien">
+            Créer un article
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
