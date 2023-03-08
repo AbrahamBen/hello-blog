c98dfc47dc05884a91f531e23c954fc494e23c01
AbrahamBen <eternelle.vision@outlook.fr>
2023-03-08 15:39:05 +0100

    React click event

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,21 @@
 const Home = () => {
+    const handlerClikc =()=>{
+        console.log('you are clicked me');
+    };
+
+    const handlerClikcAgain =(name, event)=>{
+        console.log(`Hello, ${name}`,event.target);
+    };
     return ( 
         <div className="home">
             <h2>Notre page d'accueil</h2>
+            <button onClick={handlerClikc}>
+                Ckick me
+            </button>
+
+            <button onClick={(e)=>handlerClikcAgain('Ben Abrahm',e)}>
+                Ckick me agin
+            </button>
         </div>
      );
 }
